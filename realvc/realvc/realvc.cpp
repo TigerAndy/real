31beb39228e3d4ae14b27ef440e782b981f9336e
andy <xubunn@gmail.com>
2012-12-05 10:25:27 +0900

    real vc

---
+++ realvc/realvc/realvc.cpp
@@ -0,0 +1,14 @@
+// realvc.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	cout<<"hello world";
+	return 0;
+}
+
